328010628fa3f6c203d89321dbb6140e45b10357
vincenzopiconee <vincen.picone@studenti.unina.it>
2024-12-09 16:09:13 +0100

    Initial Commit

---
+++ MyNapoli/MyNapoliApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNapoliApp.swift
+//  MyNapoli
+//
+//  Created by Vincenzo Picone on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyNapoliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
